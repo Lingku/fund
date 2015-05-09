aaa3774391861b4cae15998c0cbb6bbc408b804d
Lingku <lingku0323@gmail.com>
2015-05-09 21:12:02 +0800

    add parallel urlfetch support

--- fund.py
+++ fund.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 #
 # Copyright 2007 Google Inc.
 #
@@ -14,45 +14,80 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+from google.appengine.api import urlfetch
 import webapp2
-import urllib2
 import urllib
 import time
 
+def handle_context(context):
+    return context
+
+def handle_result(rpc, response, fund_list):
+    try:
+        result = rpc.get_result()
+    except urlfetch.DownloadError:
+        #response.write('Oops! Timeout!')
+        response.set_status(500)   # set as 500 Internal Server Error
+        return
+    
+    status_code = result.status_code
+    #response.write(status_code)
+    if status_code != 200:
+        response.write('Oops! Error Code: %d' % (status_code,))
+        response.set_status(500)   # set as 500 Internal Server Error
+        #return
+
+    #response.set_status(500, "Error!")
+    context = result.content
+    #response.write(context[:-1])
+    try:
+        fund = eval(context[:-1])
+    except TypeError:
+        fund = {}
+    fund_list.append(fund)
+    #return fund
+    #response.write(fund)
+    #response.write(lambda : context)
+
+# Use a helper function to define the scope of the callback.
+def create_callback(rpc, response, fund_list):
+    return lambda: handle_result(rpc, response, fund_list)
+
 class FundIndex(webapp2.RequestHandler):
     def get(self):
-        cb = self.request.get('cb')
-        fc = self.request.get('fc')
-        timeout = float(self.request.get('timeout'))
-
-        #self.response.write("fc=%s, cb=%s, timeout=%s" % (fc, cb, timeout))
-
+        #cb = self.request.get('cb')
+        fcs = self.request.get('fcs').split(',')
+        timeout = float(self.request.get('timeout', default_value=5))
         url = 'http://fund.eastmoney.com/data/funddataforgznew.aspx'
         #url = 'http://fund.eastmoney.com/api/ZXZT.ashx?m=0&fcodes=213008,519069,163409,320012&fileds=FSRQ,&sortfile=FCODE&sorttype=asc&callback='
-        params = urllib.urlencode({'t': 'basewap', 'cb': cb, 'fc': fc})
 
-        try:
-            time1 = time.time()
-            f = urllib2.urlopen(url, params, timeout)
+        rpcs = []
+        fund_list = []
+        #self.response.write(url)
+        #self.response.write(fcs)
+
+        for fc in fcs:
+            #self.response.write("fc=%s, cb=%s, timeout=%f\n" % (fc, cb, timeout))
+            form_fields = {
+                't': 'basewap',
+                'cb': handle_context.__name__,
+                'fc': fc
+            }
+            form_data  = urllib.urlencode(form_fields)
+            rpc = urlfetch.create_rpc(timeout)
+            rpc.callback = create_callback(rpc, self.response, fund_list)
+            urlfetch.make_fetch_call(rpc, url, payload=form_data, method=urlfetch.POST)
+            rpcs.append(rpc)
+
+        # ...
+        #self.response.set_status(404)
+        # Finish all RPCs, and let callbacks process the results.
+        for rpc in rpcs:
+            rpc.wait()
             
-        except Exception, e:
-            time2 = time.time()
-            self.response.write("time1=%lf, time1=%lf, delta time=%lf\n" % (time1, time2, time2-time1))
-            #self.response.write(e)
-            self.response.write('Oops! Timeout!')
-            self.response.set_status(500)   # set as 500 Internal Server Error
-            return
-        
-        code = f.getcode()
-        #self.response.write(code)
-        if code != 200:
-            self.response.write('Oops! Error Code: %d' % (code,))
-            self.response.set_status(500)   # set as 500 Internal Server Error
-            return
-
-        #self.response.set_status(500, "Error!")
-        context = f.read()
-        self.response.write(context)
+        #for fund in fund_list:
+        self.response.write(fund_list)
 
 app = webapp2.WSGIApplication([
     ('/fund', FundIndex),
