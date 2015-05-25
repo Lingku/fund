d8da8df1fd18a2521daeae0cc89f63c4e27a7444
Lingku <lingku0323@gmail.com>
2015-05-25 23:04:36 +0800

    temp commit

--- db.py
+++ db.py
@@ -104,28 +104,6 @@ class DbIndex(webapp2.RequestHandler):
         self.response.write('</body>')
         self.response.write('</html>')
         
-class DbRecord(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('<html>')
-        self.response.write('<head></head>')
-        self.response.write('<body>')
-    
-
-        self.response.out.write("""
-        <form action="db/rec/add" method="post">
-            <div>fund code:<input type="text" name="fc" value="000711"></div>
-            <div>fund name:<input type="text" name="name" value="fund  aaa"></div>
-            <div>isredemptive:<input type="checkbox" name="isredemptive"></div>
-            <div>fund date:<input type="date" name="date"></div>
-            <div>fund base:<input type="text" name="base"></div>
-            <div>fund units:<input type="text" name="units"></div>
-            <div>fund rate:<input type="text" name="rate"></div>
-            <div><input type="submit" value="Add a new fund record"></div>
-        </form>
-          """ )
-        self.response.write('</body>')   
-        self.response.write('</html>')
-        
 class Add(webapp2.RequestHandler):
     def get(self):
         fc = self.request.get('fc')
@@ -153,8 +131,6 @@ class Add(webapp2.RequestHandler):
             """ % (fund.fc, fund.name, fund.base, fund.units, fund.rate))
             self.response.write('Add Failed: Record Exist!')
             return
-    
-
         
         if self.request.get('base').isalnum():
             base = float(self.request.get('base'))
@@ -184,9 +160,12 @@ class Add(webapp2.RequestHandler):
                      rate=rate
                      )
         fund.put()
-        
 
         self.response.write('Add/Update OK!')
+
+class Update(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('not support!')
         
 class Delete(webapp2.RequestHandler):
     def get(self):
@@ -198,27 +177,94 @@ class DeleteAll(webapp2.RequestHandler):
         for fund in funds:
             fund.delete()
         self.response.write('delete all success!')
-        
+
 class Query(webapp2.RequestHandler):
     def get(self):
+        self.response.write('<html>')
+        self.response.write("""
+            <head>
+                <style>
+                table, th, td {
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                }
+                th, td {
+                    padding: 5px;
+                    text-align: left;
+                }
+                table#t01 {
+                    width: 100%;    
+                    background-color: #f1f1c1;
+                }
+                </style>
+            </head>
+            """)
+        self.response.write('<body>')
+        self.response.out.write("""
+            <table>
+                <tr>
+                    <th>fund code</th>
+                    <th>fund name</th> 
+                    <th>fund base</th>
+                    <th>fund units</th>
+                    <th>fund rate</th>
+                    <th>Action</th>
+                </tr>
+            """)
+            
         fc = self.request.get('fc')
         if fc is '':
             funds = db.GqlQuery("SELECT * FROM FundInfo")
-            self.response.write("aaa")
         else:
             funds = db.GqlQuery("SELECT * FROM FundInfo WHERE fc = :1", fc)
-            self.response.write(fc)
-        for fund in funds:
-            fc = fund.fc
-            name = fund.name
-            self.response.write("fc=%s, name=%s, base=%d, units=%d, rate=%f\n" % (fund.fc, fund.name, fund.base, fund.units, fund.rate))
 
+        for fund in funds:
+            self.response.out.write("""
+                <tr>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td><input type="text" disabled value="%f"></td>
+                    <td>%f</td>
+                    <td>%f</td>
+                    <td><input type="button" value="Modify?"><input type="button" value="Delete?"></td>
+                </tr>
+            """ % (fund.fc, fund.name, fund.base, fund.units, fund.rate))
+        self.response.out.write("""
+        </table>
+            """)
+
+        self.response.write('</body>')
+        self.response.write('</html>')
+        
+            
+class DbRecord(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('<html>')
+        self.response.write('<head></head>')
+        self.response.write('<body>')
+    
+
+        self.response.out.write("""
+        <form action="db/rec/add" method="post">
+            <div>fund code:<input type="text" name="fc" value="000711"></div>
+            <div>fund name:<input type="text" name="name" value="fund  aaa"></div>
+            <div>isredemptive:<input type="checkbox" name="isredemptive"></div>
+            <div>fund date:<input type="date" name="date"></div>
+            <div>fund base:<input type="text" name="base"></div>
+            <div>fund units:<input type="text" name="units"></div>
+            <div>fund rate:<input type="text" name="rate"></div>
+            <div><input type="submit" value="Add a new fund record"></div>
+        </form>
+          """ )
+        self.response.write('</body>')   
+        self.response.write('</html>')
+            
 app = webapp2.WSGIApplication([
     ('/db/?', DbIndex),
     ('/db/add', Add),
     ('/db/del', Delete),
     ('/db/delall', DeleteAll),
-    #('/db/update', Update),
+    ('/db/update', Update),
     ('/db/query', Query),
     ('/db/rec', DbRecord),
 ], debug=True)
